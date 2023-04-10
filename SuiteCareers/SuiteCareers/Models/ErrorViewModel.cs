1ec58cbd92d4ac3000bc72114d15fec8183ee0c4
tylerjacobwilson <83513320+tylerjacobwilson@users.noreply.github.com>
2023-04-10 14:44:14 -0700

    Added users model

--- SuiteCareers/SuiteCareers/Models/ErrorViewModel.cs
+++ SuiteCareers/SuiteCareers/Models/ErrorViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace SuiteCareers.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuiteCareers.Models;
 
 public class ErrorViewModel
 {
@@ -7,3 +9,16 @@ public class ErrorViewModel
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
 
+public class Users {
+    
+    [Key, DataType(DataType.EmailAddress)]
+    public string? email { get; set; }
+    [Required(ErrorMessage = "Please enter your first name")]
+    public string? firstName { get; set; }
+    [Required(ErrorMessage = "Please enter your last name")]
+    public string? lastName { get; set; }
+    [Required(ErrorMessage = "Please enter the city of which you live")]
+    public string? city { get; set; }
+    [Required(ErrorMessage = "Please Enter the state of which you live")]
+    public string? state { get; set; }   
+        }
