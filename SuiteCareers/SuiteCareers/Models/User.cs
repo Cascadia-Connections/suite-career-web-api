9e82b0622e5dfe1487ed78cd6e936d86782c76ec
Tyler Wilson <tylerjacobwilson@gmail.com>
2023-04-18 16:06:21 -0700

    Removed requirements for model class user

--- SuiteCareers/SuiteCareers/Models/User.cs
+++ SuiteCareers/SuiteCareers/Models/User.cs
@@ -9,13 +9,13 @@ namespace SuiteCareers.Models
 
             [Key, DataType(DataType.EmailAddress)]
             public string? Email { get; set; }
-            [Required(ErrorMessage = "Please enter your first name")]
+            
             public string? FirstName { get; set; }
-            [Required(ErrorMessage = "Please enter your last name")]
+            
             public string? LastName { get; set; }
-            [Required(ErrorMessage = "Please enter the city of which you live")]
+            
             public string? City { get; set; }
-            [Required(ErrorMessage = "Please Enter the state of which you live")]
+            
             public string? State { get; set; }
         }
 
@@ -31,3 +31,57 @@ namespace SuiteCareers.Models
 
     }
 
+            public DateTime date { get; set; }
+            //How to add foreigh key to this class?
+            //[DataType(DataType.EmailAddress)]
+            //public string? Email { get; set; }
+
+        }
+        public class Session
+        {
+            [Key]
+            public int sessionId { get; set; }
+            [Required(ErrorMessage = "Please enter the date")]
+            public DateTime date { get; set; }
+            [Required]
+            public int interviewId { get; set; }
+
+            public string email { get; set; }
+
+            
+        }
+
+        public class Interview
+        {
+            [Key]
+            public int interviewId { get; set; }
+
+
+            [Required]
+            public int questionId { get; set; }
+
+
+        }
+
+        public class Question
+        {
+            [Key]
+            public int questionId { get; set; }
+
+            [Required]
+            public string? question { get; set; }
+        }
+            
+        public class Response
+        {
+            [Key]
+            public int responseId { get; set; }
+            public int response { get; set; }
+            
+            public int questionId { get; set; }
+        }
+
+
+
+    }
+}
