543cf14b05e74d26a496c6bab7c97b24dda4d7c0
darthcod3r <thenoahku@gmail.com>
2023-04-25 15:03:34 -0700

    seed data completed

--- SuiteCareers/SuiteCareers/Data/Migrations/20230425215907_seed-data.Designer.cs
+++ SuiteCareers/SuiteCareers/Data/Migrations/20230425215907_seed-data.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SuiteCareers.Data;
 
@@ -10,9 +11,11 @@ using SuiteCareers.Data;
 namespace SuiteCareers.Data.Migrations
 {
     [DbContext(typeof(SuiteCareersDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230425215907_seed-data")]
+    partial class seeddata
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.3");
