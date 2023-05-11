688d7e209815941d7de9a2213c20d83ddfb5add6
snholmes <shelbynholmes@gmail.com>
2023-05-10 19:09:53 -0700

    TABLES WORKING

--- SuiteCareers/SuiteCareers/Controllers/HomeController.cs
+++ SuiteCareers/SuiteCareers/Controllers/HomeController.cs
@@ -1,50 +1,56 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SuiteCareers.Data;
 using SuiteCareers.Models;
 using System.Diagnostics;
 
-namespace SuiteCareers.Controllers;
 
-public class HomeController : Controller
+namespace SuiteCareers.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-    private SuiteCareersDbContext _db;
 
-    public HomeController(ILogger<HomeController> logger, SuiteCareersDbContext db)
+    public class HomeController : Controller
     {
-        _db = db;
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
+        private SuiteCareersDbContext _db;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public HomeController(ILogger<HomeController> logger, SuiteCareersDbContext db)
+        {
+            _db = db;
+            _logger = logger;
+        }
 
-    public IActionResult UserAnalytics()
-    {
-        return View();
-    }
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    public IActionResult UserDemographics()
-    {
-        return View();
-    }
+        public IActionResult UserAnalytics()
+        {
+            return View();
+        }
 
-    public IActionResult Sessions()
-    {
-        return View();
-    }
+        public IActionResult UserDemographics()
+        {
+            return View();
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        [HttpGet]
+        public IActionResult Sessions()
+        {
+            IQueryable<Session> allSessions = _db.Sessions.Include(s => s.User).OrderByDescending(s => s.Date);
+            return View(allSessions);
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
 
