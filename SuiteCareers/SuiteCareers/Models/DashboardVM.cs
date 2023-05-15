a6c366f61739f21d38b6f67c270fd826d3e108e0
Chad <chadloswald@gmail.com>
2023-05-15 14:31:25 -0700

    Adding IEnumerables from all models to the VM

--- SuiteCareers/SuiteCareers/Models/DashboardVM.cs
+++ SuiteCareers/SuiteCareers/Models/DashboardVM.cs
@@ -4,5 +4,10 @@ namespace SuiteCareers.Models
 {
     public class DashboardVM
     {
+        public IEnumerable<User>? Users { get; set; }
+        public IEnumerable<Session>? Sessions { get; set; }
+        public IEnumerable<Response>? Responses { get; set; }
+        public IEnumerable<Question>? Questions { get; set; }
+        public IEnumerable<Interview>? Interviews { get; set; }
     }
 }
