06021e9a006f8db332e7c03472f0eca00c38219a
Chad <chadloswald@gmail.com>
2023-06-01 12:41:27 -0700

    Trying to fix the merge

--- SuiteCareers/SuiteCareers/Controllers/HomeController.cs
+++ SuiteCareers/SuiteCareers/Controllers/HomeController.cs
@@ -1,20 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SuiteCareers.Models;
 using SuiteCareers.Data;
-<<<<<<< HEAD
 using Microsoft.EntityFrameworkCore;
 using SuiteCareers.ViewModels;
 using System.Linq;
-=======
-using SuiteCareers.Models;
-using System.Diagnostics;
->>>>>>> 0455ca21fd77f792508b57503413a43117132882
 
-namespace SuiteCareers.Controllers
+namespace SuiteCareers.Controllers;
+
+public class HomeController : Controller
 {
+    private readonly ILogger<HomeController> _logger;
+    private SuiteCareersDbContext _db;
 
-    public class HomeController : Controller
+    public HomeController(ILogger<HomeController> logger, SuiteCareersDbContext db)
     {
-<<<<<<< HEAD
         _db = db;
         _logger = logger;
     }
@@ -68,120 +68,31 @@ namespace SuiteCareers.Controllers
             
             return View(dashboardVM);
     }
-=======
-        private readonly ILogger<HomeController> _logger;
-        private SuiteCareersDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger, SuiteCareersDbContext db)
-        {
-            _db = db;
-            _logger = logger;
-        }
->>>>>>> 0455ca21fd77f792508b57503413a43117132882
+    public IActionResult Questions()
+    {
+        return View();
+    }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+    public IActionResult Sessions()
+    {
+        return View();
+    }
 
-        public IActionResult Questions()
-        {
-            return View();
-        }
+    public IActionResult Users()
+    {
+        return View();
+    }
 
-        public IActionResult UserDemographics()
-        {
-            return View();
-        }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
 
-        public IActionResult Users()
-        {
-            return View();
-        }
-
-        public IActionResult Sessions(
-            string term = "",
-            string orderBy = "",
-            int currentPage = 1,
-            DateTime? start = null,
-            DateTime? end = null
-        )
-        {
-            term = string.IsNullOrEmpty(term) ? "" : term.ToLower();
-            var sessionData = new SessionsVM();
-
-            sessionData.UserSortParm = orderBy == "user" ? "user_desc" : "user";
-            sessionData.DateSortParm = orderBy == "date" ? "date_desc" : "date";
-            sessionData.TimeSortParm = orderBy == "time" ? "time_desc" : "time";
-            sessionData.SessionIDSortParm = orderBy == "session_id" ? "session_id_desc" : "session_id";
-
-            var sessions = _db.Sessions.Where(session =>
-            (term == "" ||
-            session.User.LastName.ToLower().StartsWith(term) ||
-            session.User.FirstName.ToLower().StartsWith(term) ||
-            session.SessionId.ToString().StartsWith(term)) &&
-            (!start.HasValue || session.StartDate >= start.Value) &&
-            (!end.HasValue || session.StartDate <= end.Value))
-            .Select(session => new Session()
-            {
-                SessionId = session.SessionId,
-                StartDate = session.StartDate,
-                EndDate = session.EndDate,
-                User = session.User
-            });
-
-            switch (orderBy)
-            {
-                case "user":
-                    sessions = sessions.OrderBy(s => s.User.LastName).ThenBy(s => s.User.FirstName);
-                    break;
-                case "user_desc":
-                    sessions = sessions.OrderByDescending(s => s.User.LastName).ThenBy(s => s.User.FirstName);
-                    break;
-                case "session_id_desc":
-                    sessions = sessions.OrderByDescending(s => s.SessionId);
-                    break;
-                case "session_id":
-                    sessions = sessions.OrderBy(s => s.SessionId);
-                    break;
-                case "date":
-                    sessions = sessions.OrderBy(s => s.StartDate);
-                    break;
-                case "time":
-                    sessions = sessions.OrderBy(s => s.StartDate.TimeOfDay);
-                    break;
-                case "time_desc":
-                    sessions = sessions.OrderByDescending(s => s.StartDate.TimeOfDay);
-                    break;
-                default:
-                    sessions = sessions.OrderByDescending(s => s.StartDate);
-                    break;
-            }
-            int totalRecords = sessions.Count();
-            int pageSize = 10;
-            int totalPages = (int)Math.Ceiling((double)totalRecords / pageSize);
-            sessions = sessions.Skip((currentPage - 1) * pageSize).Take(pageSize);
-            sessionData.Sessions = sessions;
-            sessionData.CurrentPage = currentPage;
-            sessionData.TotalPages = totalPages;
-            sessionData.Term = term;
-            sessionData.PageSize = pageSize;
-            sessionData.OrderBy = orderBy;
-            sessionData.Start = start;
-            sessionData.End = end;
-            return View(sessionData);
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 
 }
