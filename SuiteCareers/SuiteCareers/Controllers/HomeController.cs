13a87c2a05c8979f2f54380d4a5a910b24d146ad
Chad <chadloswald@gmail.com>
2023-06-03 11:33:12 -0700

    Added content that was missing back into HomeController

--- SuiteCareers/SuiteCareers/Controllers/HomeController.cs
+++ SuiteCareers/SuiteCareers/Controllers/HomeController.cs
@@ -74,9 +74,77 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Sessions()
+    public IActionResult Sessions(
+    string term = "",
+    string orderBy = "",
+    int currentPage = 1,
+    DateTime? start = null,
+    DateTime? end = null
+)
     {
-        return View();
+        term = string.IsNullOrEmpty(term) ? "" : term.ToLower();
+        var sessionData = new SessionsVM();
+
+        sessionData.UserSortParm = orderBy == "user" ? "user_desc" : "user";
+        sessionData.DateSortParm = orderBy == "date" ? "date_desc" : "date";
+        sessionData.TimeSortParm = orderBy == "time" ? "time_desc" : "time";
+        sessionData.SessionIDSortParm = orderBy == "session_id" ? "session_id_desc" : "session_id";
+
+        var sessions = _db.Sessions.Where(session =>
+        (term == "" ||
+        session.User.LastName.ToLower().StartsWith(term) ||
+        session.User.FirstName.ToLower().StartsWith(term) ||
+        session.SessionId.ToString().StartsWith(term)) &&
+        (!start.HasValue || session.StartDate >= start.Value) &&
+        (!end.HasValue || session.StartDate <= end.Value))
+        .Select(session => new Session()
+        {
+            SessionId = session.SessionId,
+            StartDate = session.StartDate,
+            EndDate = session.EndDate,
+            User = session.User
+        });
+
+        switch (orderBy)
+        {
+            case "user":
+                sessions = sessions.OrderBy(s => s.User.LastName).ThenBy(s => s.User.FirstName);
+                break;
+            case "user_desc":
+                sessions = sessions.OrderByDescending(s => s.User.LastName).ThenBy(s => s.User.FirstName);
+                break;
+            case "session_id_desc":
+                sessions = sessions.OrderByDescending(s => s.SessionId);
+                break;
+            case "session_id":
+                sessions = sessions.OrderBy(s => s.SessionId);
+                break;
+            case "date":
+                sessions = sessions.OrderBy(s => s.StartDate);
+                break;
+            case "time":
+                sessions = sessions.OrderBy(s => s.StartDate.TimeOfDay);
+                break;
+            case "time_desc":
+                sessions = sessions.OrderByDescending(s => s.StartDate.TimeOfDay);
+                break;
+            default:
+                sessions = sessions.OrderByDescending(s => s.StartDate);
+                break;
+        }
+        int totalRecords = sessions.Count();
+        int pageSize = 10;
+        int totalPages = (int)Math.Ceiling((double)totalRecords / pageSize);
+        sessions = sessions.Skip((currentPage - 1) * pageSize).Take(pageSize);
+        sessionData.Sessions = sessions;
+        sessionData.CurrentPage = currentPage;
+        sessionData.TotalPages = totalPages;
+        sessionData.Term = term;
+        sessionData.PageSize = pageSize;
+        sessionData.OrderBy = orderBy;
+        sessionData.Start = start;
+        sessionData.End = end;
+        return View(sessionData);
     }
 
     public IActionResult Users()
